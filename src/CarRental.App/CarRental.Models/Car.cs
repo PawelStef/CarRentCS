46d445a37deb74be38b0e20f278af81fdb740dcd
unknown <paw.stefanowski@gmail.com>
2020-10-10 11:09:51 +0200

    first commit

---
+++ src/CarRental.App/CarRental.Models/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.Models
+{
+    public class Car
+    {
+        public string PlateId { get; set; }
+
+        public Segment Segment { get; set; }
+
+        public double Price { get; set; }
+
+        public List<Reservation> ReservationHistory { get; set; }
+
+
+    }
+}
